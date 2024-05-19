4e14716a89aafbb8f13c435de39697efbc0a52da
MASKUN2 <maskunflower2@naver.com>
2024-05-20 01:07:48 +0900

    Initial commit

---
+++ src/main/kotlin/com/maskun/dartfinance/DartfinanceApplication.kt
@@ -0,0 +1,11 @@
+package com.maskun.dartfinance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DartfinanceApplication
+
+fun main(args: Array<String>) {
+    runApplication<DartfinanceApplication>(*args)
+}
